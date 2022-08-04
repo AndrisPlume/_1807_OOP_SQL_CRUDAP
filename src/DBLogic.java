dd920b5aa5ba0c63f2bf04196b2dde3651f69734
Andris Plume <andris.plume@gmail.com>
2022-08-04 16:36:56 +0300

    Final commit

--- src/DBLogic.java
+++ src/DBLogic.java
@@ -1,2 +1,132 @@
-package PACKAGE_NAME;public class DBLogic {
-}
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Scanner;
+
+
+public class DBLogic {
+
+    // Establish connection to SQL
+    String DB = "jdbc:mysql://localhost:3306/07_crudap";
+    String USER = "root";
+    String PASS = "Tornakalna7.";
+
+    Scanner scan = new Scanner(System.in);
+
+    // insert encapsulated encapsulated for limited use
+    private void insert() {
+
+        try {
+
+            Connection conn = DriverManager.getConnection(DB, USER, PASS);
+            Statement stmt = conn.createStatement();
+
+            String sql = "INSERT INTO users (email, name) VALUES ('andris.plume@gmail.lv', 'Andris')";
+
+            stmt.executeUpdate(sql);
+
+            System.out.println("Dati ir ievadīti");
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // Getter for insert function
+    public void getInsert() {
+        insert();
+    }
+    // delete function encapsulated for limited use
+
+    private void delete() {
+
+        try {
+
+            Connection conn = DriverManager.getConnection(DB, USER, PASS);
+            Statement stmt = conn.createStatement();
+            System.out.println("Insert user Nr. to delete");
+            int x = scan.nextInt();
+
+            String sql = "DELETE FROM users WHERE id ='" + x + "'";
+
+            stmt.executeUpdate(sql);
+
+            System.out.println("Dati ir izdzēsti");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    // Getter for delete funcion
+
+    public void getDelete() {
+        delete();
+
+    }
+
+    // update
+    private void update() {
+
+        try {
+
+            Connection conn = DriverManager.getConnection(DB, USER, PASS);
+            Statement stmt = conn.createStatement();
+
+            System.out.println("Insert user Nr. to update");
+            int y = scan.nextInt();
+
+            System.out.println("Enter new email:");
+            String nemail = scan.next();
+            System.out.println("enter new name:");
+            String nname = scan.next();
+
+            String sql = "UPDATE users SET email = '"+ nemail +"', name ='"+ nname +"' WHERE id ='" + y + "'";
+
+            stmt.executeUpdate(sql);
+
+            System.out.println("Dati ir atjaunoti");
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //getter for update
+
+        public void getUpdate() {
+            update();
+        }
+
+            // select
+            private void select () {
+
+                try {
+
+                    Connection conn = DriverManager.getConnection(DB, USER, PASS);
+                    Statement stmt = conn.createStatement();
+                    ResultSet rs = stmt.executeQuery("SELECT id, email, name FROM users");
+
+
+                    while (rs.next()) {
+                        System.out.println("id: " + rs.getInt("id"));
+                        System.out.println("email: " + rs.getString("email"));
+                        System.out.println("name: " + rs.getString("name"));
+                        System.out.println("");
+
+
+                    }
+
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            public void getSelect () {
+                    select();
+                }
+            }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        DBLogic db = new DBLogic();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number Insert=1; Delete=2; Update=3; Select=4");
+        int Darbiba  = scan.nextInt();
+
+        switch (Darbiba) {
+            case 1:
+                db.getInsert();
+                break;
+            case 2:
+                db.getDelete();
+                break;
+            case 3:
+                db.getUpdate();
+                break;
+            case 4:
+                db.getSelect();
+                break;
+
+        }
+
     }
 }
